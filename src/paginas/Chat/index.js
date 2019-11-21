7bded75f7e83ebb58a72468b919a3d99b033fb21
Luciana Leite <lprimante@gmail.com>
2019-11-21 16:54:40 -0300

    Melhorar legilibidade

--- src/paginas/Chat/index.js
+++ src/paginas/Chat/index.js
@@ -3,19 +3,15 @@ import Mensagem from '../../componentes/Mensagem'
 import { getMensagens } from '../../service/mensagens'
 // Importe de estilo
 import './styles.css'
-
+//Hooks
 function Chat () {
   const [mensagens, setMensagens] = useState(undefined)
-
-  function handleStatusChange (mensagens) {
-    setMensagens(mensagens)
-  }
 
   useEffect(() => {
     setTimeout(
       getMensagens()
         .then(response => {
-          handleStatusChange(response.data)
+          setMensagens(response.data)
         })
         .catch(error => {
           console.error(error)
